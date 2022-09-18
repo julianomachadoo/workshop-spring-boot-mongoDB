b35e4fc8b21c0a52275ba5efc2d961f7e2a311fe
Juliano Machado <julmachadoramos@gmail.com>
2022-09-18 13:54:21 -0300

    Commit inicial, projeto criado

---
+++ src/test/java/com/github/julianomachadoo/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.julianomachadoo.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
