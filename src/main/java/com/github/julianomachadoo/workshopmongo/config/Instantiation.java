923ea4629063b78954c1f948b750d4695cefe316
Juliano Machado <julmachadoramos@gmail.com>
2022-09-19 13:50:14 -0300

    referenciando os posts do usuario

--- src/main/java/com/github/julianomachadoo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/github/julianomachadoo/workshopmongo/config/Instantiation.java
@@ -35,7 +35,10 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, LocalDate.of(2018, 03, 21), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, LocalDate.of(2018, 03, 23), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
-        
+
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
