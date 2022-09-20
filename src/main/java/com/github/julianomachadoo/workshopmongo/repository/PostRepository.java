5bd29952977c18057ac388e49c6d8f6568322c68
Juliano Machado <julmachadoramos@gmail.com>
2022-09-20 09:04:16 -0300

    Consulta por título

--- src/main/java/com/github/julianomachadoo/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/github/julianomachadoo/workshopmongo/repository/PostRepository.java
@@ -1,10 +1,13 @@
 package com.github.julianomachadoo.workshopmongo.repository;
 
 import com.github.julianomachadoo.workshopmongo.domain.Post;
-import com.github.julianomachadoo.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/github/julianomachadoo/workshopmongo/resources/PostResources.java
+++ src/main/java/com/github/julianomachadoo/workshopmongo/resources/PostResources.java
@@ -3,9 +3,11 @@ package com.github.julianomachadoo.workshopmongo.resources;
 import com.github.julianomachadoo.workshopmongo.domain.Post;
 import com.github.julianomachadoo.workshopmongo.domain.User;
 import com.github.julianomachadoo.workshopmongo.dto.UserDTO;
+import com.github.julianomachadoo.workshopmongo.resources.util.URL;
 import com.github.julianomachadoo.workshopmongo.services.PostService;
 import com.github.julianomachadoo.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -26,5 +28,12 @@ public class PostResources {
         public ResponseEntity<Post> findById(@PathVariable String id) {
                 Post post = service.findById(id);
                 return ResponseEntity.ok().body(post);
+        }
+
+        @GetMapping(value = "/titlesearch")
+        public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text ) {
+                text = URL.decodeParam(text);
+                List<Post> list = service.findByTitle(text);
+                return ResponseEntity.ok().body(list);
         }
 }
